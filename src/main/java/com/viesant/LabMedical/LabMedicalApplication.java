c3804502781b26d831a239a0dce649fddf99096b
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-08-10 22:34:30 +0200

    Start project

---
+++ src/main/java/com/viesant/LabMedical/LabMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.viesant.LabMedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabMedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viesant/LabMedical/LabMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viesant.LabMedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
